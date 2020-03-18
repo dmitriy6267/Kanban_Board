264d9a31ff35492127ba6cd1fbe8de804b41ec08
dmitriy6267 <dmitriy6267@gmail.com>
2020-03-18 13:48:43 +0800

    Updates

---
+++ precache-manifest.8f3edb58d59ddaa5461843da7b376915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849f2d0880d8b392b381c8368cc7d9e9",
+    "url": "/Kanban_Board/index.html"
+  },
+  {
+    "revision": "da3bdfc6c47d707607e9",
+    "url": "/Kanban_Board/static/css/main.655fb535.chunk.css"
+  },
+  {
+    "revision": "99ae5a16ad69fe3638cf",
+    "url": "/Kanban_Board/static/js/2.caf6c1cf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Kanban_Board/static/js/2.caf6c1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3bdfc6c47d707607e9",
+    "url": "/Kanban_Board/static/js/main.fb4cf6dd.chunk.js"
+  },
+  {
+    "revision": "8f9807cfb113a9b7a6be",
+    "url": "/Kanban_Board/static/js/runtime-main.e5343418.js"
+  }
+]);
